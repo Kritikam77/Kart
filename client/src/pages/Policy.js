23345d01b931f2f90e27effab5b3b128f218afa1
Kritika <kritikam77@gmail.com>
2023-06-01 16:14:55 +0530

    1

---
+++ client/src/pages/Policy.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const Policy = () => {
+  return (
+    <Layout title={"Privacy Policy"}>
+      <div className="row contactus " style={{margin:"2rem"}}>
+        <div className="col-md-6 ">
+          <img
+            src="/images/privacy.svg"
+            alt="contactus"
+            style={{ width: "100%",margin:"2rem" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Policy;
